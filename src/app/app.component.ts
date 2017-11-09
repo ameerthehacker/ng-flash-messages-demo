5505716f6801888ace6abe6128627433ebde5a78
ameerthehacker <ameerjhanprof@gmail.com>
2017-11-09 18:22:16 +0530

    Bring ng-flash-messages to life along with few UI tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import {
   Validators
 } from '@angular/forms';
 
+import { NgFlashMessageService } from 'ng-flash-messages';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,19 +19,37 @@ export class AppComponent implements OnInit {
 
   flashForm: FormGroup;
 
+  constructor(private ngFlashMessageService: NgFlashMessageService) {}
+
   ngOnInit() {
     this.flashForm = new FormGroup({
       message: new FormControl("Hey there i'm a flash message", Validators.required),
       timeout: new FormControl('2000'),
       type: new FormControl('info'),
-      dismissible: new FormControl('yes')
+      dismissible: new FormControl('no')
     });
   }
   get message(): FormControl {
     return <FormControl>this.flashForm.get('message');
   }
+  get timeout(): FormControl {
+    return <FormControl>this.flashForm.get('timeout');
+  }
+  get type(): FormControl {
+    return <FormControl>this.flashForm.get('type');
+  }
+  get dismissible(): FormControl {
+    return <FormControl>this.flashForm.get('dismissible');
+  }
   onFlashFormSubmit() {
+    let flashMessage = {
+      messages: [this.message.value],
+      timeout: this.timeout.value,
+      type: this.type.value,
+      dismissible: this.dismissible.value == 'yes'? true: false
+    }
 
+    this.ngFlashMessageService.showFlashMessage(flashMessage);
   }
 
 }

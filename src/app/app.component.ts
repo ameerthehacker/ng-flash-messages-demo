5711a67f31c166cdb733b08f5f06f1d13e199907
ameerthehacker <ameerjhanprof@gmail.com>
2017-11-09 18:11:45 +0530

    Add missing dismissible field to the form and make the form reactive using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { 
+  Component, 
+  OnInit 
+} from '@angular/core';
+import { 
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  flashForm: FormGroup;
+
+  ngOnInit() {
+    this.flashForm = new FormGroup({
+      message: new FormControl("Hey there i'm a flash message", Validators.required),
+      timeout: new FormControl('2000'),
+      type: new FormControl('info'),
+      dismissible: new FormControl('yes')
+    });
+  }
+  get message(): FormControl {
+    return <FormControl>this.flashForm.get('message');
+  }
+  onFlashFormSubmit() {
+
+  }
+
 }
